c99eb95d466487c6a57d429ef8415f97d1e797c9
Ali Lakhia <lakhia@gmail.com>
2025-04-27 00:44:15 -0700

    Fix bug where bottom of listView was being truncated below the screen

--- app/src/main/java/com/creationsahead/speedwordsearch/ui/LevelActivity.java
+++ app/src/main/java/com/creationsahead/speedwordsearch/ui/LevelActivity.java
@@ -3,11 +3,11 @@ package com.creationsahead.speedwordsearch.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.FrameLayout;
 import com.creationsahead.speedwordsearch.R;
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.MaterialToolbar;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 /**
  * Level selection activity
@@ -20,10 +20,18 @@ public class LevelActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.level);
+
         levelListView = createLevelListView();
-        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-        FrameLayout frameLayout = findViewById(R.id.main_grid);
-        frameLayout.addView(levelListView, params);
+        ConstraintLayout topLayout = findViewById(R.id.topLayout);
+        AppBarLayout appBarLayout = findViewById(R.id.appBarLayout2);
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.MATCH_PARENT, 0);
+        params.topToBottom = appBarLayout.getId();
+        params.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
+        params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
+        params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID;
+        topLayout.addView(levelListView, params);
+
         MaterialToolbar materialToolbar = findViewById(R.id.toolbar);
         materialToolbar.setTitle(R.string.app_name);
     }
